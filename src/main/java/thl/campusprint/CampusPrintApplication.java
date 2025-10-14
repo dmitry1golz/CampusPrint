d2551fa748a1749bd55b06fc4fc5528a572402bb
Dmtry Golz <dmsg2017@outlook.com>
2025-10-14 16:29:10 +0300

    add springboot template

---
+++ src/main/java/thl/campusprint/CampusPrintApplication.java
@@ -0,0 +1,13 @@
+package thl.campusprint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampusPrintApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CampusPrintApplication.class, args);
+    }
+
+}
---
+++ src/test/java/thl/campusprint/CampusPrintApplicationTests.java
@@ -0,0 +1,13 @@
+package thl.campusprint;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CampusPrintApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
