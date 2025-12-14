183fa238569a64c722ab49a61f4fd721dfadaf32
Raphael Risse <raphael.risse@stud.th-luebeck.de>
2025-12-14 20:13:30 +0100

    fix index links (add temporary link for testing)

--- src/main/resources/static/scripts/main.ts
+++ src/main/resources/static/scripts/main.ts
@@ -1,18 +1,18 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const bookingButtons = document.querySelectorAll('.btn.primary');
-    const viewDevicesButton = document.querySelector('.btn.secondary');
+    const bookingButtons = document.querySelectorAll('#buchenButton');
+    const viewDevicesButton = document.querySelector('#geraeteButton');
 
     bookingButtons.forEach(button => {
         button.addEventListener('click', () => {
-            alert('Weiterleitung zur Buchungsseite...');
-            // window.location.href = '/buchung'; // Füge den richtigen Pfad ein
+            // TODO nur fürs testen der buchen seite, sollte eigentlich auch auf Druckerauswahl gehen?
+            // TODO oder soll die buchen seite ohne vorher ausgewählten drucker funktionieren
+            window.location.href = '/buchung.html?geraet_id=b856dff6-f4f8-4af1-bb44-888561eb584d';
         });
     });
 
     if (viewDevicesButton) {
         viewDevicesButton.addEventListener('click', () => {
-            alert('Zeige verfügbare Geräte...');
-            // window.location.href = '/geraete'; // Füge den richtigen Pfad ein
+            window.location.href = '/geraete.html';
         });
     }
 });
