6b571eb28fa92d1be358a7cd57789344397ebb72
Raphael Risse <raphael.risse@stud.th-luebeck.de>
2025-12-14 19:52:44 +0100

    implement state of buchen seite (loading, error and ready with printer information)

--- src/main/resources/static/scripts/buchung.ts
+++ src/main/resources/static/scripts/buchung.ts
@@ -1,51 +1,88 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const form = document.getElementById('bookingForm') as HTMLFormElement;
-    const printerInfo = document.getElementById('printerInfo')!;
-    const urlParams = new URLSearchParams(window.location.search);
-    const geraet = urlParams.get('geraet');
+import { createNewBooking } from "./services/buchung-service.js";
+import { getGeraetById } from "./services/geraet-service.js";
+import { setCookie } from "./utils.js";
 
-    // Den Namen des Druckers anzeigen
-    if (geraet) {
-        printerInfo.innerHTML = `<h2>${decodeURIComponent(geraet)}</h2><p class="printer-sub">Hohe Qualität – Automatisches Bedleveling</p>`;
+type BuchenPageState = 'loading' | 'error' | 'ready';
+
+// ---------- Constant Elements -----------
+const loadingStateElement = document.getElementById('pageState-loading');
+const errorStateElement = document.getElementById('pageState-error');
+const readyStateElement = document.getElementById('pageState-ready');
+
+const form = document.getElementById('bookingForm') as HTMLFormElement;
+const printerInfo = document.getElementById('printerInfo')!;
+// ----------------------------------------
+
+document.addEventListener('DOMContentLoaded', () => {
+    const urlParams = new URLSearchParams(window.location.search);
+
+    // Loading Device Data
+    const geraetId = urlParams.get('geraet_id');
+    if (!geraetId) {
+        updateState('error');
+        return;
     }
+    const geraet = getGeraetById(geraetId!);
+    if (!geraet) {
+        updateState('error');
+        return;
+    }
+
+    // Set Printer Data
+    document.getElementById('printerInfo-Name')!.innerText=geraet.name;
+    document.getElementById('printerInfo-Description')!.innerText=geraet.description;
 
     // Formular bearbeiten
     form.addEventListener('submit', (e) => {
         e.preventDefault();
 
+        // TODO name not used
         const name = (document.getElementById('name') as HTMLInputElement).value;
         const email = (document.getElementById('email') as HTMLInputElement).value;
         const date = (document.getElementById('date') as HTMLInputElement).value;
         const start = (document.getElementById('start') as HTMLInputElement).value;
         const end = (document.getElementById('end') as HTMLInputElement).value;
         const notes = (document.getElementById('notes') as HTMLTextAreaElement).value;
-
-        if (!geraet) {
-            alert("Kein Gerät ausgewählt!");
-            return;
-        }
-
+        
+        // Validate Data
         if (start >= end) {
             alert("Die Endzeit muss nach der Startzeit liegen.");
             return;
         }
 
-        const booking = {
-            geraet,
-            name,
-            email,
-            date,
-            start,
-            end,
-            notes,
-        };
+        const booking: NewPrintBooking = {
+            printerId: geraet.id,
+            date: date,
+            time: `${start} - ${end}`,
+            notes: notes, // treat empty string as null?
+        }
 
-        console.log("📤 Buchung wird gesendet:", booking);
+        createNewBooking(booking);
 
-        // TODO: fetch('/api/buchung', { method: 'POST', body: JSON.stringify(booking) })
-
-        alert("Buchung erfolgreich abgesendet!");
         form.reset();
-        window.location.href = "index.html"; 
+
+        setCookie('userEmail', email, 30)
+
+        window.location.href = "meine-drucke.html"; 
     });
+
+    // Page ready
+    updateState('ready')
 });
+
+function updateState(newState: BuchenPageState) {
+    loadingStateElement!.hidden = true;
+    errorStateElement!.hidden = true;
+    readyStateElement!.hidden = true;
+    switch(newState) {
+        case "loading":
+            loadingStateElement!.hidden = false;
+            break;
+        case "error":
+            errorStateElement!.hidden = false;
+            break;
+        case "ready":
+            readyStateElement!.hidden = false;
+            break;
+    }
+}
