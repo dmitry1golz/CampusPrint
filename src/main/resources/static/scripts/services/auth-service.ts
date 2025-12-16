f474a30a8dc9eaf1874a89202e983799468eb9d6
Paul Kamm <paul.kamm@stud.th-luebeck.de>
2025-12-16 12:25:32 +0100

    moved cookie sevices into authservice

--- src/main/resources/static/scripts/services/auth-service.ts
+++ src/main/resources/static/scripts/services/auth-service.ts
@@ -1,4 +1,3 @@
-import { setCookie, getCookie, deleteCookie } from '../utils.js';
 
 const COOKIE_NAME = 'admin_session';
 
@@ -33,4 +32,29 @@ export function requireAuth() {
         console.warn("Zugriff verweigert. Redirect zum Login.");
         window.location.href = 'admin-login.html';
     }
+}
+
+export function setCookie(name: string, value: string, days: number) {
+    let expires = "";
+    if (days) {
+        const date = new Date();
+        date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+        expires = "; expires=" + date.toUTCString();
+    }
+    document.cookie = name + "=" + (value || "") + expires + "; path=/";
+}
+
+export function getCookie(name: string): string | null {
+    const nameEQ = name + "=";
+    const ca = document.cookie.split(';');
+    for (let i = 0; i < ca.length; i++) {
+        let c = ca[i];
+        while (c.charAt(0) == ' ') c = c.substring(1, c.length);
+        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
+    }
+    return null;
+}
+
+export function deleteCookie(name: string) {
+    document.cookie = name + '=; Path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT;';
 }
