8b991e4df69b1852bd79acab88687739a405516b
Rafael Bergmann <rafael.bergmann@stud.th-luebeck.de>
2025-12-21 00:11:30 +0100

    Rename user column in bookings table H2 don't like column named user.

--- src/main/java/thl/campusprint/models/Booking.java
+++ src/main/java/thl/campusprint/models/Booking.java
@@ -19,7 +19,7 @@ public class Booking {
     private Integer id;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "user", nullable = false)
+    @JoinColumn(name = "user_id", nullable = false)
     private User user;
 
     @ManyToOne(fetch = FetchType.LAZY)
