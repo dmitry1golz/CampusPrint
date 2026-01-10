0d3f1a18b137946bb3e7f76b22d0218c5e6b8372
Raphael Risse <raphael.risse@stud.th-luebeck.de>
2026-01-10 13:09:29 +0100

    updating frontend models to be able to handle selected options

--- src/main/resources/static/scripts/models/booking.ts
+++ src/main/resources/static/scripts/models/booking.ts
@@ -1,3 +1,5 @@
+import { FdmMaterial, LaserPreset, SlaMaterial } from "./device";
+
 // Export Bookinginterface
 export interface Booking {
     id: string;
@@ -12,13 +14,65 @@ export interface Booking {
     status: 'pending' | 'confirmed' | 'completed' | 'rejected' | 'running';
 }
 
-export interface NewBooking {
+export type SupportType = 'none' | 'touching_bed' | 'everywhere' | 'auto';
+
+export interface SelectedFdmOptions {
+    tech_type: 'FDM';
+    selected_material: FdmMaterial;
+    selected_layer_height: number;
+    selected_nozzle_size: number;
+    selected_support_type: SupportType;
+    selected_infill_percentage: number;
+}
+
+export interface SelectedSlaOptions {
+    tech_type: 'SLA';
+    selected_material: SlaMaterial;
+    selected_layer_height: number;
+    selected_support_type: SupportType;
+    selected_infill_percentage: number;
+}
+
+export interface SelectedLaserOptions {
+    tech_type: 'LASER';
+    selected_preset: LaserPreset;
+}
+
+export interface SelectedPaperOptions {
+    tech_type: 'PAPER';
+    selected_paper_weights: number;
+    selected_format: string;
+}
+
+export interface BaseNewBooking {
     printerId: String;
     userEmail: string;
     startDate: Date;
     endDate: Date;
     notes?: string;
 }
+
+export interface NewFDMPrinterBooking extends BaseNewBooking {
+    type: 'FDM_Printer';
+    print_options: SelectedFdmOptions;
+}
+
+export interface NewSLAPrinterBooking extends BaseNewBooking {
+    type: 'SLA_Printer';
+    print_options: SelectedSlaOptions;
+}
+
+export interface NewLaserCutterBooking extends BaseNewBooking {
+    type: 'Laser_Cutter';
+    print_options: SelectedLaserOptions;
+}
+
+export interface NewPaperPrinterBooking extends BaseNewBooking {
+    type: 'Printer'; 
+    print_options: SelectedPaperOptions;
+}
+
+export type NewBooking = NewFDMPrinterBooking | NewSLAPrinterBooking | NewLaserCutterBooking | NewPaperPrinterBooking;
 
 export interface BookingAvailability {
     blockedWeekDays: number[];
