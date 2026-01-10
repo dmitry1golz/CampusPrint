24a151131ed6c40b6ab62044e5ba13c9791dc5c3
Raphael Risse <raphael.risse@stud.th-luebeck.de>
2026-01-10 21:34:30 +0100

    allow enum to be null

--- src/main/java/thl/campusprint/models/options/SlaSelectedOptions.java
+++ src/main/java/thl/campusprint/models/options/SlaSelectedOptions.java
@@ -2,12 +2,14 @@ package thl.campusprint.models.options;
 
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 
 @Data
 @EqualsAndHashCode(callSuper = true)
 public class SlaSelectedOptions extends PrintJobSlectedOptions {
     private SlaMaterial selected_material;
     private Double selected_layer_height;
+    @JsonDeserialize(using = SupportTypeDeserializer.class)
     private SupportType selected_support_type;
     private Double selected_infill_percentage;
 }
