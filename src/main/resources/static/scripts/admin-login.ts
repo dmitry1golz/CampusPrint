1756a8a125b909c807d477c9879ec45d816d60b6
Dmitry Golz <golz.dmitry@gmail.com>
2026-01-11 00:18:33 +0100

    simplified the code a little

--- src/main/resources/static/scripts/admin-login.ts
+++ src/main/resources/static/scripts/admin-login.ts
@@ -1,38 +1,41 @@
-import { login, isAuthenticated } from './services/auth-service.js';
+import {isAuthenticated, login} from './services/auth-service.js';
 
-if (isAuthenticated()) {
-    window.location.href = 'admin.html';
-}
+document.addEventListener('DOMContentLoaded', async () => {
 
-document.addEventListener('DOMContentLoaded', () => {
-    const form = document.getElementById('adminLoginForm') as HTMLFormElement;
-    const emailInput = document.getElementById('email') as HTMLInputElement;
-    const passwordInput = document.getElementById('password') as HTMLInputElement;
-    const errorBanner = document.getElementById('login-error') as HTMLDivElement;
-
-    if (form) {
-        form.addEventListener('submit', (e) => {
-            e.preventDefault();
-            
-            if(errorBanner) errorBanner.classList.add('hidden');
-
-            const email = emailInput.value;
-            const password = passwordInput.value;
-
-            const success = login(email, password);
-
-            if (success) {
-                window.location.href = 'admin.html';
-            } else {
-                if (errorBanner) {
-                    errorBanner.classList.remove('hidden');
-                } else {
-                    alert('Falsche Zugangsdaten!');
-                }
-                
-                passwordInput.value = '';
-                passwordInput.focus();
-            }
-        });
+    if (await isAuthenticated()) {
+        window.location.href = 'admin.html';
+        return;
     }
+
+    const form = document.getElementById('adminLoginForm') as HTMLFormElement | null;
+    const emailInput = document.getElementById('email') as HTMLInputElement | null;
+    const passwordInput = document.getElementById('password') as HTMLInputElement | null;
+    const errorBanner = document.getElementById('login-error') as HTMLDivElement | null;
+
+    if (!form || !emailInput || !passwordInput) return;
+
+
+    form.addEventListener('submit', async (e) => {
+        e.preventDefault();
+
+        errorBanner?.classList.add('hidden');
+
+        const email = emailInput.value.trim();
+        const password = passwordInput.value;
+
+        const success = await login(email, password);
+
+        if (success) {
+            window.location.href = 'admin.html';
+            return;
+        }
+
+        if (errorBanner) errorBanner.classList.remove("hidden");
+        else alert("Falsche Zugangsdaten!");
+
+        passwordInput.value = '';
+        passwordInput.focus();
+
+    });
+
 });
